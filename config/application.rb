c3cfd2b8f28eb4921eab8ee23aebaaa03d40657d
ArturKioshi <artur.kioshi1234@gmail.com>
2026-01-16 23:40:12 -0300

    chore: setup database.yml with environment variables and docker support

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,10 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+if defined?(Dotenv)
+  Dotenv::Rails.load
+end
+
 module Backend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
